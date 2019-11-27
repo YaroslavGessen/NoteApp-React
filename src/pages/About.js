068ab1948e84c501a08d7b1b573a76dc3139a104
Yaroslavessen <ygessen@gmail.com>
2019-11-27 21:34:27 +0200

    my comment

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => (
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-4">Note App</h1>
+        <p class="lead">Version of app <strong>1.0.11</strong>
+        </p>
+    </div>
+</div>
+)
